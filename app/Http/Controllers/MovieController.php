3b9feea2f9bcfdc16d1e830e04493c7e64a4d027
Eliabraham <eliud.abraham.jobs@gmail.com>
2023-11-10 15:05:41 -0300

    acciones en movie control

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,10 +1,17 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
+    namespace App\Http\Controllers;
+    use Illuminate\Http\Request;
+    use App\Models\Movie as peli;
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $peliculas = peli::all();
+        return view ('ruta.vista',['peliculas'=>$peliculas]);
+    }
+    public function create(){}
+    public function store(){}
+    public function edit(){}
+    public function update(){}
+    public function show(){}
+    public function destroy(){}
 }
