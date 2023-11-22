e44d1e80725d597d3b783917e3e2426749890626
Eliabraham <eliud.abraham.jobs@gmail.com>
2023-11-22 18:57:51 -0300

    operaciones de clientes menos eliminar

--- app/Livewire/Customer/Lista.php
+++ app/Livewire/Customer/Lista.php
@@ -5,33 +5,62 @@
     class Lista extends Component{
         public $clientes="";
         public $db_operation="";
-        public $cliente_especifico;
         public $name;
         public $identity;
         public $status;
         public $birth;
         public $interest;
+        public $clt;
         public function create(){
             $this->db_operation="crear";
-            unset($this->cliente_especifico);
+            $this->name="";
+            $this->identity="";
+            $this->status="";
+            $this->birth="";
+            $this->interest="";
         }
         public function mount(){
             $this->clientes=mod_cliente::all();
         }
         public function modificar($id){
-            $this->cliente_especifico=mod_cliente::find($id);
+            $cliente_especifico =mod_cliente::find($id);
+            $this->name         =$cliente_especifico->name;
+            $this->identity     =$cliente_especifico->identity;
+            $this->status       =$cliente_especifico->status;
+            $this->birth        =$cliente_especifico->birth;
+            $this->interest     =$cliente_especifico->interest;
+            $this->clt      =$id;
             $this->db_operation="modificar";
         }
         public function store(){
-            
+            $clie=new mod_cliente;
+            $clie->name=$this->name;
+            $clie->identity=$this->identity;
+            $clie->status=$this->status;
+            $clie->birth=$this->birth;
+            $clie->interest=$this->interest;
+            $clie->save();
+            $this->clientes=mod_cliente::all();
+            $this->name="";
+            $this->identity="";
+            $this->status="";
+            $this->birth="";
+            $this->interest="";
         }
         public function update($id){
-            
+            $clie = mod_cliente::find($id);
+            $clie->name     =$this->name;
+            $clie->identity =$this->identity;
+            $clie->status   =$this->status;
+            $clie->birth    =$this->birth;
+            $clie->interest =$this->interest;
+            $clie->save();
+            $this->db_operation="";
+            $this->clientes=mod_cliente::all();
         }
         public function render(){
             return view('livewire.customer.lista')
                 ->extends('layout.master')
                 ->section('contenido');
         }
-
     }
--- resources/views/livewire/customer/nuevo.blade.php
+++ resources/views/livewire/customer/nuevo.blade.php
@@ -1,33 +1,33 @@
 @csrf
 <div class="form-group col-5 d-inline-block"> 
     <label for="name">Nombre</label>
-    <input class="form-control form-control-sm" type="search" name="name" id="name" maxlength="30" required value="{{ isset($cliente_especifico) ? $cliente_especifico->name: '' }}" wire:model="name"/>
+    <input class="form-control form-control-sm" type="search" name="name" id="name" maxlength="30" required wire:model="name"/>
 </div>
 <div class="form-group col-5 d-inline-block"> 
     <label for="duration">Documento de Identidad</label>
-    <input class="form-control form-control-sm" value="{{ isset($cliente_especifico) ? $cliente_especifico->identity: '' }}" type="number" name="identity" id="identity" required wire:model="identity"/>
+    <input class="form-control form-control-sm" type="number" name="identity" id="identity" required wire:model="identity"/>
 </div>
 <div class="form-group col-5 d-inline-block"> 
     <label for="status">Estatus</label>
     <select class="form-control form-control-sm" name="status" id="status" required wire:model="status">
         <option value="">--</option>
-        <option {{ isset($cliente_especifico) && $cliente_especifico->status == 'inactivo' ? 'selected' : '' }}>inactivo</option>
-        <option {{ isset($cliente_especifico) && $cliente_especifico->status == 'activo' ? 'selected' : '' }}>activo</option>
-        <option {{ isset($cliente_especifico) && $cliente_especifico->status == 'amonestado' ? 'selected' : '' }}>amonestado</option>
-        <option {{ isset($cliente_especifico) && $cliente_especifico->status == 'betado' ? 'selected' : '' }}>betado</option>
+        <option>inactivo</option>
+        <option>activo</option>
+        <option>amonestado</option>
+        <option>betado</option>
     </select>
 </div>
 <div class="form-group col-5 d-inline-block"> 
     <label for="birth">Dia de Nacimiento</label>
-    <input class="form-control form-control-sm" value="{{ isset($cliente_especifico) ? $cliente_especifico->birth: '' }}" type="date" name="birth" id="birth" wire:model="birth"/>
+    <input class="form-control form-control-sm" type="date" name="birth" id="birth" wire:model="birth"/>
 </div>
 <div class="form-group col-10 d-inline-block"> 
     <label for="interest">Intereses</label>
-    <textarea class="form-control form-control-sm" cols="30" rows="3" name="interest" id="interest" wire:model="interest" required>{{ isset($cliente_especifico) ? $cliente_especifico->interest: '' }}</textarea>
+    <textarea class="form-control form-control-sm" cols="30" rows="3" name="interest" id="interest" wire:model="interest" required></textarea>
 </div>
 @if ($db_operation=='crear')
-    <button>Aceptar</button>
+    <button wire:click="store()">Aceptar</button>
 @else
-    <button wire:click="update({{ $cliente_especifico->id }})">Modificar</button>
+    <button wire:click="{{ 'update('.$clt.')' }}">Modificar</button>
 @endif
 <button wire:click="$set('db_operation','')">Cancelar</button>
