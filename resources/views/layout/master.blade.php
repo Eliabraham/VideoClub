6266f2f73ea993c56211f5e94050dbb3d8a6a739
Eliabraham <eliud.abraham.jobs@gmail.com>
2023-11-23 11:06:15 -0300

    estabilidad del sistema entre la ruta de clientes y la ruta de videos

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>..::VIDEO CLUB::..</title>
+        <script src="{{ asset('js/js.js') }}"></script>
         @livewireStyles
     </head>
     <body>
--- resources/views/livewire/movie/nueva.blade.php
+++ resources/views/livewire/movie/nueva.blade.php
@@ -1,57 +1,62 @@
-<script src="{{ asset('js/js.js') }}"></script>
 @csrf
 <div class="form-group col-5 d-inline-block"> 
     <label for="name">Nombre</label>
-    <input class="form-control form-control-sm" type="search" name="name" id="name" autocomplete="off" maxlength="30" required value="{{ isset($pelicula) ? $pelicula->name: '' }}"/>
+    <input class="form-control form-control-sm" type="search" name="name" id="name" autocomplete="off" maxlength="30" required wire:model="name"/>
 </div>
 <div class="form-group col-3 d-inline-block"> 
     <label for="duration">Duración</label>
-    <input class="form-control form-control-sm" value="{{ isset($pelicula) ? $pelicula->duration: '' }}" type="number" name="duration" id="duration" required/>
+    <input class="form-control form-control-sm" wire:model="duration" type="number" name="duration" id="duration" required/>
 </div>
 <div class="form-group col-3 d-inline-block"> 
     <label for="status">Estatus</label>
-    <input type="search" name="status" id="status" autocomplete="off" value="{{ isset($pelicula) ? $pelicula->status: '' }}" maxlength="12" class="form-control form-control-sm" required/>
+    <input type="search" name="status" id="status" autocomplete="off" wire:model="status" maxlength="12" class="form-control form-control-sm" required/>
 </div>
 <div class="form-group col-5 d-inline-block"> 
     <label for="director">Director</label>
-    <input class="form-control form-control-sm" autocomplete="off" value="{{ isset($pelicula) ? $pelicula->director: '' }}" type="search" name="director" id="director" maxlength="30" required/>
+    <input class="form-control form-control-sm" autocomplete="off" wire:model="director" type="search" name="director" id="director" maxlength="30" required/>
 </div>
 <div class="form-group col-3 d-inline-block"> 
     <label for="genre">Genero</label>
-    <input class="form-control form-control-sm" autocomplete="off" value="{{ isset($pelicula) ? $pelicula->genre: '' }}" type="search" name="genre" id="genre" maxlength="30" required/>
+    <input class="form-control form-control-sm" autocomplete="off" wire:model="genre" type="search" name="genre" id="genre" maxlength="30" required/>
 </div>
 <div class="form-group col-3 d-inline-block"> 
     <label for="classification">Clasificación</label>
-    <select name="classification" id="classification" class="form-control form-control-sm" required>
-        <option value="" {{ isset($pelicula) && $pelicula->classification == '' ? 'selected' : '' }}>--</option>
-        <option value="U" {{ isset($pelicula) && $pelicula->classification == 'U' ? 'selected' : '' }}>Todo Público</option>
-        <option value="PG" {{ isset($pelicula) && $pelicula->classification == 'PG' ? 'selected' : '' }}>Documentales</option>
-        <option value="12A" {{ isset($pelicula) && $pelicula->classification == '12A' ? 'selected' : '' }}>Mayores de 12</option>
-        <option value="15A" {{ isset($pelicula) && $pelicula->classification == '15A' ? 'selected' : '' }}>Mayores de 15</option>
-        <option value="18A" {{ isset($pelicula) && $pelicula->classification == '18A' ? 'selected' : '' }}>Mayores de 18</option>
-        <option value="18R" {{ isset($pelicula) && $pelicula->classification == '18R' ? 'selected' : '' }}>Reservado</option>
+    <select name="classification" id="classification" class="form-control form-control-sm" required wire:model="classification">
+        <option value="" >--</option>
+        <option value="U">Todo Público</option>
+        <option value="PG">Documentales</option>
+        <option value="12A">Mayores de 12</option>
+        <option value="15A">Mayores de 15</option>
+        <option value="18A">Mayores de 18</option>
+        <option value="18R">Reservado</option>
     </select>    
 </div>
 <div class="form-group col-11 d-inline-block"> 
     <label for="synopsis">Synopsis</label>
-    <textarea name="synopsis" id="synopsis" autocomplete="off" maxlength="300" class="form-control form-control-sm" required>{{ isset($pelicula) ? $pelicula->synopsis: '' }}</textarea>
+    <textarea name="synopsis" id="synopsis" autocomplete="off" maxlength="300" class="form-control form-control-sm" required wire:model="synopsis"></textarea>
 </div>
 <div class="form-group col-4 d-inline-block"> 
     <label for="existence">Existencia</label>
-    <input type="number" name="existence" id="existence" autocomplete="off" value="{{ isset($pelicula) ? $pelicula->existence: '' }}"  min="1" max="100" required class="form-control form-control-sm"/>
+    <input type="number" name="existence" id="existence" autocomplete="off" wire:model="existence"  min="1" max="100" required class="form-control form-control-sm"/>
 </div>
 <div class="form-group col-4 d-inline-block"> 
     <label for="availability">Disponibilidad</label>
-    <input type="number" name="availability" id="availability" autocomplete="off" value="{{ isset($pelicula) ? $pelicula->availability: '' }}"  min="0" max="100" required class="form-control form-control-sm" />
+    <input type="number" name="availability" id="availability" autocomplete="off" wire:model="availability"  min="0" max="100" required class="form-control form-control-sm" />
 </div>
 <div class="form-group col-3 d-inline-block">
     <label for="poster" class="selector_imagen">Seleccionar Imagen de Portada</label>
     @if(isset($pelicula))
-    <input type="file" class="inputImagen" name="poster" id="poster" onchange="previewImage(event, '#imgPreview')"/>
+        <input type="file" class="inputImagen" name="poster" id="poster" wire:model="poster" onchange="previewImage(event, '#imgPreview')"/>
     @else
-    <input type="file" class="inputImagen" name="poster" id="poster" required onchange="previewImage(event, '#imgPreview')"/>
+        <input type="file" class="inputImagen" name="poster" id="poster" wire:model="poster" required onchange="previewImage(event, '#imgPreview')"/>
     @endif
 </div>
 <div id="imagen">
     <img id="imgPreview" class="imagenPreview" src="{{ isset($pelicula) ? '/img/poster/'.$pelicula->poster: '' }}" alt="Imagen Seleccionada">
 </div>
+@if ($db_operation=='create_movie')
+    <button wire:click="store()">Aceptar</button>
+@else
+    <button wire:click="{{ 'update()' }}">Modificar</button>
+@endif
+<button wire:click="$set('db_operation','')">Cancelar</button>
